e545e493d08804d9fafddd7866c8b002f0f9d7db
IanoNjuguna <inc3099@gmail.com>
2026-07-30 21:36:38 +0300

    find a tx in block 216351 that spends the coinbase output of block 216128

--- submission/03.sh
+++ submission/03.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/bash
 # Which tx in block 216,351 spends the coinbase output of block 216,128?
+TXID=$(
+	bitcoin-cli -signet getblock \
+		"$(bitcoin-cli -signet getblockhash 216128)" |
+		jq -r '.tx[0]'
+	)
+	
+bitcoin-cli -signet getblock \
+	"$(bitcoin-cli -signet getblockhash 216351)" 2 |
+	jq -r --arg txid "$TXID" \
+	'.tx[] | select(any(.vin[]; .txid == $txid)) | .txid'
+
