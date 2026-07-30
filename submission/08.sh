98ca46befd1b14ebf2cc1fd5e1520f3499acabac
IanoNjuguna <inc3099@gmail.com>
2026-07-30 22:52:12 +0300

    find block height a tx was mined in

--- submission/08.sh
+++ submission/08.sh
@@ -1,2 +1,11 @@
+#!/usr/bin/bash
 # what block height was this tx mined ?
 # 49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb
+BLOCK_HASH=$(
+	bitcoin-cli -signet getrawtransaction \
+		49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb true |
+		jq -r '.blockhash'
+)
+
+bitcoin-cli -signet getblockheader "$BLOCK_HASH" |
+	jq '.height'
