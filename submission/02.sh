ec088d3c6b94a25149457d8147ffad5697f7e7c7
IanoNjuguna <inc3099@gmail.com>
2026-07-30 21:28:14 +0300

    get UTXOs created by a particular block

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,6 @@
+#!/usr/bin/bash
 # How many new outputs were created by block 243,825?
+BLOCK_HASH=$(bitcoin-cli -signet getblockhash 243825)
+bitcoin-cli -signet getblock "$BLOCK_HASH" 2 |
+	jq '[.tx[].vout[]] | length'
+
